4c94a887a842def98a29237c1b10706ded3cbc7c
Mouli <sreekar.mouli1998@gmail.com>
2017-06-04 13:17:37 +0530

    Added some visual improvements

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
 function Clock(x, y) {
-
+	var txt_size;
 	this.x = x;
 	this.y = y;
 	this.h = {
@@ -42,13 +42,14 @@ function Clock(x, y) {
 		}
 	};
 	if (windowWidth - this.x < windowHeight - this.y) {
-		this.s.length = windowWidth - this.x - 10;
+		this.s.length = (windowWidth - this.x) * 0.75;
+		txt_size = map(this.s.length, 0, (windowWidth - this.y) * 0.75, 72, 15);
 	} else {
-		this.s.length = windowHeight - this.y - 5;
+		this.s.length = (windowHeight - this.y) * 0.75;
+		txt_size = map(this.s.length, 0, (windowHeight - this.y) * 0.75, 72, 15);
 	}
-	this.m.length = this.s.length * 90 / 100;
+	this.m.length = this.s.length;
 	this.h.length = this.m.length * 60 / 100;
-
 
 
 	this.render = function (x, y) {
@@ -57,27 +58,37 @@ function Clock(x, y) {
 			console.log("Mobile");
 		}
 
-
+		//	GETTING THE HOUR, MINUTE AND SECOND VALUE
 		this.h.value = hour();
 		this.m.value = minute();
 		this.s.value = second();
 
+		//	FINDING THE ANGLES
+		//	HOUR ANGLE
 		this.h.angle = map(this.h.value, 0, 24 / 2, -PI / 2, 3 * PI / 2);
+		//	MINUTE ANGLE
 		this.m.angle = map(this.m.value, 0, 60, -PI / 2, 3 * PI / 2);
+		//	SECOND ANGLE
 		this.s.angle = map(this.s.value, 0, 60, -PI / 2, 3 * PI / 2);
 
-		var temp = map(this.m.value / 12, 0, 60 / 12, 0, 5);
+		//	FINDING THE HOUR'S OFFSET ANGLE
+		var offset = map(this.m.value / 12, 0, 60 / 12, 0, 5);
 
+		//	ADDING OFFSET ANGLE TO HOUR'S ANGLE
 		if (this.h.angle > 0) {
-			this.h.angle += temp * PI / 30;
+			this.h.angle += offset * PI / 30;
 		} else if (this.h.angle < 0) {
-			this.h.angle -= temp * PI / 30;
+			this.h.angle -= offset * PI / 30;
 		}
 
+		//	FINDING THE END POINTS OF HOUR, MINUTE AND SECOND
+		//	HOUR
 		this.h.end.x = this.h.length * cos(this.h.angle);
 		this.h.end.y = this.h.length * sin(this.h.angle);
+		//	MINUTE
 		this.m.end.x = this.m.length * cos(this.m.angle);
 		this.m.end.y = this.m.length * sin(this.m.angle);
+		//	SECOND
 		this.s.end.x = this.s.length * cos(this.s.angle);
 		this.s.end.y = this.s.length * sin(this.s.angle);
 
@@ -87,41 +98,79 @@ function Clock(x, y) {
 	}
 
 	this.draw = function () {
-		push();
 		translate(this.x, this.y);
-		noFill();
-		stroke(255);
-		strokeWeight(7);
-		
+
+
 		/*arc(0, 0, 2 * this.s.length, 2 * this.s.length, -PI / 2, this.s.angle);
 		arc(0, 0, 2 * this.m.length, 2 * this.m.length, -PI / 2, this.m.angle);
 		arc(0, 0, 2 * this.h.length, 2 * this.h.length, -PI / 2, this.h.angle);*/
-		
-		ellipse(0, 0, 2 * this.s.length, 2 * this.s.length);
-		
-		/*ellipse(0, 0, 2 * this.m.length, 2 * this.m.length);
-		ellipse(0, 0, 2 * this.h.length, 2 * this.h.length);*/
 
-		/*for (var i = 0; i < 12; i++) {
+		/*
+		ellipse(0, 0, 2 * this.m.length, 2 * this.m.length);
+		ellipse(0, 0, 2 * this.h.length, 2 * this.h.length);
+		*/
+
+		textAlign(CENTER, CENTER);
+		textSize(txt_size);
+		noStroke();
+		for (var i = 0; i < 12; i++) {
 			var x = this.s.length * cos(map(i, 0, 12, -PI / 2, 3 * PI / 2));
 			var y = this.s.length * sin(map(i, 0, 12, -PI / 2, 3 * PI / 2));
-			text(".", x, y);
+			if (i == 0) {
+				hr = "60";
+			} else if (i == 1) {
+				hr = "05";
+			} else if (i == 2) {
+				hr = "10";
+			} else if (i == 3) {
+				hr = "15";
+			} else if (i == 4) {
+				hr = "20";
+			} else if (i == 5) {
+				hr = "25";
+			} else if (i == 6) {
+				hr = "30";
+			} else if (i == 7) {
+				hr = "35";
+			} else if (i == 8) {
+				hr = "40";
+			} else if (i == 9) {
+				hr = "45";
+			} else if (i == 10) {
+				hr = "50";
+			} else if (i == 11) {
+				hr = "55";
+			}
+			text(hr, x * 1.1, y * 1.1);
 		}
-		
-		textSize(1);
-		for (var i = 0; i < 60; i++) {
+
+		/*for (var i = 0; i < 60; i++) {
 			var x = this.m.length * cos(map(i, 0, 60, -PI / 2, 3 * PI / 2));
 			var y = this.m.length * sin(map(i, 0, 60, -PI / 2, 3 * PI / 2));
 			text(".", x, y);
 		}*/
 
-		fill(255);
-		stroke(255);
+		noFill();
+		stroke(75, 0, 211);
+		strokeWeight(10);
+
+		ellipse(0, 0, (2 * this.s.length) + (0.5 * this.s.length), (2 * this.s.length) + (0.5 * this.s.length));
+
+		strokeWeight(7);
+		fill(75, 0, 211);
+		stroke(75, 0, 211);
+		ellipse(0, 0, 20, 20);
+
 		line(this.h.start.x, this.h.start.y, this.h.end.x, this.h.end.y);
+
 		line(this.m.start.x, this.m.start.y, this.m.end.x, this.m.end.y);
+
+
 		strokeWeight(3);
+		//stroke(255, 250, 250);
+
 		line(this.s.start.x, this.s.start.y, this.s.end.x, this.s.end.y);
+
 		line(this.s.start.x, this.s.start.y, -this.s.length * 0.25 * cos(this.s.angle), -this.s.length * 0.25 * sin(this.s.angle));
-		pop();
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var startX;
 var startY;
-var tm = [];
+var tm;
 var clock_ding;
 var bgcolor = {
 	r: 51,
@@ -16,20 +16,17 @@ function preLoad() {
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	//cb = new CheckBox();
-	tm.push(new Clock(width / 2, height / 2));
+	tm = new Clock(width / 2, height / 2);
 }
 
 function draw() {
-	background(bgcolor.r, bgcolor.g, bgcolor.b);
-	for (i = 0; i < tm.length; i++) {
-		tm[i].render();
-	}
+	background(255);
+	//background(bgcolor.r, bgcolor.g, bgcolor.b);
+	tm.render();
 }
 
 function mouseDragged() {
-	//tm.push(new Clock(mouseX, mouseY));
-	bgcolor.r = map(mouseX, 0, width, 0 ,255);
-	bgcolor.b = map(mouseY, 0, height, 0 ,255);
-	
-	
+	bgcolor.r = map(mouseX, 0, width, 0, 255);
+	bgcolor.g = map(mouseY, 0, height, 0, 255);
+	bgcolor.b = map(mouseX, 0, width, 255, 0);
 }
